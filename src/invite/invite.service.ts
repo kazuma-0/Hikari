f97e06ac1518621c814795b2d897dbb88f046b5c
kazuma-0 <as8644459@gmail.com>
2022-10-04 20:26:59 +0530

    Add method to batch generate invite codes

--- src/invite/invite.service.ts
+++ src/invite/invite.service.ts
@@ -10,11 +10,27 @@ export class InviteService {
     private readonly inviteCodeRepository: Repository<InviteCode>,
   ) {}
 
+  async getAllInviteCode(): Promise<InviteCode[]> {
+    const codes = await this.inviteCodeRepository.find();
+    return codes;
+  }
+
   async generateInviteCode(): Promise<InviteCode> {
     const code = referralCodeGenerator.alpha('uppercase', 8);
     return await this.inviteCodeRepository.save({
       code: code,
     });
+  }
+
+  async generateInviteCodes(count: number) {
+    const codes = [];
+    for (let i = 0; i < count; i++) {
+      const randomCount = Math.max(6, Math.floor(Math.random() * 10));
+      codes.push({
+        code: referralCodeGenerator.alpha('uppercase', randomCount),
+      });
+    }
+    return await this.inviteCodeRepository.save(codes);
   }
 
   async validateInviteCode(code: string): Promise<boolean> {
