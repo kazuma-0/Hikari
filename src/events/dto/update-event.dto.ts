4e6da9f76be3c3f61dd67ac696da0794a597e807
kazuma-0 <as8644459@gmail.com>
2022-10-07 11:54:44 +0530

    Add slug, created_at and likes

--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -2,6 +2,9 @@ import CreateEventDto from './create-event.dto';
 
 class UpdateEventDto extends CreateEventDto {
   id: number;
+  slug: string;
+  created_at: Date;
+  likes: number;
 }
 
 export default UpdateEventDto;
