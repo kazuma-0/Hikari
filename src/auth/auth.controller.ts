d13c679ed77e8940b81abfd3b61714bdfbe4f620
kazuma-0 <as8644459@gmail.com>
2022-10-07 11:52:57 +0530

    remove logging statement

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -25,7 +25,6 @@ export class AuthController {
   // @UseGuards(AuthGuard('web3'))
   // @Roles(ROLE.EXECUTIVE)
   async SignUp(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
     return await this.authService.createUser(createUserDto);
   }
 
