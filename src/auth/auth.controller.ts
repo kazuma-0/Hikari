2e0625915a8db62181b87b30c522fe52908bde2f
kazuma-0 <as8644459@gmail.com>
2022-10-09 17:00:28 +0530

    feat: add methods for updating and deleting users
    ]

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,16 +4,19 @@ import {
   Get,
   Post,
   UseGuards,
-  Req,
   Param,
+  Put,
+  Delete,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import CreateUserDto from './dto/create-user.dto';
 import LoginDto from './dto/login.dto';
+import UpdateUserDto from './dto/update-user.dto';
 import { Roles } from './role/role.decorator';
 import ROLE from './role/role.enum';
+import User from './user.entity';
 
 @Controller({
   path: 'auth',
@@ -24,26 +27,40 @@ export class AuthController {
   @Post('/new')
   // @UseGuards(AuthGuard('web3'))
   // @Roles(ROLE.EXECUTIVE)
-  async SignUp(@Body() createUserDto: CreateUserDto) {
+  async SignUp(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.authService.createUser(createUserDto);
   }
 
   @Post('/login')
-  async logIn(@Body() loginDto: LoginDto) {
+  async logIn(@Body() loginDto: LoginDto): Promise<User> {
     return await this.authService.validateUser(loginDto.pubKey);
   }
 
   @Get('/user/all')
   @Roles(ROLE.EXECUTIVE, ROLE.TEACHER)
   @UseGuards(AuthGuard('web3'))
-  async getUsers() {
+  async getUsers(): Promise<User[]> {
     return this.authService.getAllUsers();
+  }
+
+  @Put('/user/update')
+  @Roles(ROLE.EXECUTIVE, ROLE.TEACHER)
+  @UseGuards(AuthGuard('web3'))
+  async updateUser(@Body() updateUserDto: UpdateUserDto): Promise<User> {
+    return this.authService.updateUser(updateUserDto);
+  }
+
+  @Delete('/user/:id')
+  @Roles(ROLE.EXECUTIVE, ROLE.TEACHER)
+  @UseGuards(AuthGuard('web3'))
+  async deleteUser(@Param('id') id: string): Promise<boolean> {
+    return this.authService.deleteUser(id);
   }
 
   @Get('/user/:pubKey')
   @Roles(ROLE.EXECUTIVE, ROLE.TEACHER, ROLE.USER)
   @UseGuards(AuthGuard('web3'))
-  async getUser(@Param('pubKey') pubKey: string) {
+  async getUser(@Param('pubKey') pubKey: string): Promise<User> {
     return this.authService.getUserByPubKey(pubKey);
   }
 }
